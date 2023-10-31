d83ddb996f1f62abe1b0e960a75dc00850ff99e1
Tien biet bayyy <107696490+Tienhp03@users.noreply.github.com>
2023-10-31 16:04:12 +0700

    Add files via upload

---
+++ KTCOBAN021.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+#define MAX 1000000000000000001
+ull Recursive (ull result, short x, int xcount, short y, int ycount, ull n, int sochuso){
+    if(xcount + ycount > sochuso + 2 || result > MAX){
+        return MAX;
+    }
+    if(xcount == ycount && result >= n){
+        return result;
+    }
+    return min(Recursive(result * 10 + x, x, xcount + 1, y, ycount, n, sochuso), Recursive(result * 10 + y, x, xcount, y, ycount + 1, n ,sochuso));
+}
+
+ull FindNum(ull n, short x, short y){
+    int sochuso = floor(log10(n)) + 1; 
+    ull result = 0; 
+    int xcount = 0; 
+    int ycount = 0; 
+    if (x == y){
+        while (result < n){
+            result = result * 10 + x;
+        }
+        return result;
+    }
+    return Recursive(result, x, xcount, y, ycount, n, sochuso);
+}
+
+int main (){
+    int test;
+    cin>>test;
+    while (test--){
+        ull n;
+        cin>>n;
+        short x, y;
+        cin>>x>>y;
+        cout<<FindNum(n ,x ,y)<<endl;
+    }
+    return 0;
+}
